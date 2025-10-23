39cfee52bbfaad5491682eebd779d7ca990e634f
Todd Kovalsky <t.e.kovalsky@gmail.com>
2025-10-23 15:49:15 -0400

    Fix: Update contentService import to use articles index file

--- src/lib/contentService.ts
+++ src/lib/contentService.ts
@@ -18,7 +18,7 @@ interface RawTestimonial {
 
 import { AREAS } from '@/app/content/areas';
 import { DEVELOPMENTS } from '@/app/content/developments';
-import { ALL_ARTICLES as ARTICLES } from '@/app/content/articles';
+import { ALL_ARTICLES as ARTICLES } from '@/app/content/articles/index';
 import { TESTIMONIALS } from '@/app/content/testimonials';
 import { MARKET_DATA } from '@/app/content/marketData';
 
